bc39b9bfecc6b6d1e0bafe01b7e2193e65839307
Apache <pablohenrique2k2@gmail.com>
2026-05-16 21:05:55 -0300

    fix: recover bot sessions after worker restart

--- bot/src/worker.js
+++ bot/src/worker.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const { Worker } = require('bullmq');
 const config = require('./config');
 const { getRedisConnection } = require('./config/redis');
-const { QUEUE_NAMES } = require('./queues/whatsappQueues');
+const { QUEUE_NAMES, getQueues, buildJobId } = require('./queues/whatsappQueues');
+const { listActiveSessionIds, readBotState } = require('./services/botStateStore');
 const {
   processInboundMessage,
   processOutboundMessage,
@@ -45,11 +46,50 @@ const workers = [
   createWorker(QUEUE_NAMES.outbound, processOutboundMessage, config.bot.outboundConcurrency),
 ];
 
+const RECOVERABLE_SESSION_STATUSES = new Set([
+  'ready',
+  'authenticated',
+  'starting',
+  'qr_ready',
+  'disconnected',
+]);
+
+async function enqueueSessionRecoveryOnStartup() {
+  const activeSessionIds = await listActiveSessionIds().catch(() => []);
+  if (!activeSessionIds.length) return;
+
+  const queues = getQueues();
+  let queued = 0;
+
+  for (const barbeariaId of activeSessionIds) {
+    const state = await readBotState(barbeariaId).catch(() => null);
+    if (!state?.requestedPhoneNumber || !RECOVERABLE_SESSION_STATUSES.has(state.status)) {
+      continue;
+    }
+
+    await queues.session.add('reconnect', { barbeariaId }, {
+      jobId: buildJobId(['startup-reconnect', barbeariaId]),
+    });
+    queued += 1;
+  }
+
+  console.log('[BOT_WORKER] Recuperacao de sessoes enfileirada', {
+    activeSessionIds: activeSessionIds.length,
+    queued,
+  });
+}
+
 console.log('[BOT_WORKER] Workers iniciados', {
   workerId: config.bot.workerId,
   sessionConcurrency: config.bot.sessionConcurrency,
   inboundConcurrency: config.bot.inboundConcurrency,
   outboundConcurrency: config.bot.outboundConcurrency,
+});
+
+enqueueSessionRecoveryOnStartup().catch((error) => {
+  console.error('[BOT_WORKER] Falha ao enfileirar recuperacao de sessoes', {
+    message: error?.message,
+  }, error);
 });
 
 setInterval(() => {
